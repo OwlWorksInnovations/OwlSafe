6f0e4324cc03fe15fccbd4d219fdddfee18cc815
Juan <108938779+OwlWorksInnovations@users.noreply.github.com>
2025-09-12 18:01:48 +0200

    Code is going to be some much neater and type checking!

--- app.py
+++ app.py
@@ -1,263 +1,55 @@
-from PyQt5.QtWidgets import QApplication, QMainWindow, QPushButton, QVBoxLayout, QWidget, QListWidget, QLineEdit, QMessageBox, QListWidgetItem, QDialog
-from PyQt5.QtCore import Qt
-import sqlite3
-from encrypt import generate_password_hash, verify_password_hash, encrypt_password, decrypt_password, generate_key, encrypt_master_key_file, load_master_key_file
+from sys import argv
+from PyQt5.QtWidgets import QApplication, QMainWindow, QWidget, QVBoxLayout, QListWidget, QPushButton, QDialog, QLabel
 
-GLOBAL_PASSWORD = None
+class InitialWindow(QDialog):
+    def __init__(self):
+        super().__init__()
 
-def create_db():
-        conn = sqlite3.connect("passwords.db")
-        cur = conn.cursor()
-        cur.execute("""
-            CREATE TABLE IF NOT EXISTS passwords (
-                id INTEGER PRIMARY KEY AUTOINCREMENT,
-                password TEXT NOT NULL
-            )
-        """)
-        cur.execute("""
-            CREATE TABLE IF NOT EXISTS master_password (
-                password TEXT NOT NULL            
-            )
-        """)
-        conn.commit()
-        conn.close()
+        # Window paramaters
+        self.setWindowTitle("OwlSafe | Enter Master Password")
+        self.setFixedSize(400, 200)
 
-create_db()
+        # Layout setup
+        layout = QVBoxLayout()
 
-try:
-    class StartupWindow(QDialog):  
-        def __init__(self):
-            super().__init__()
-            self.setWindowTitle("OwlSafe | ENTER MASTER PASSWORD")
-            self.setFixedSize(400, 200)
-            self.setWindowFlags(Qt.Dialog | Qt.WindowTitleHint | Qt.CustomizeWindowHint)
-            
-            layout = QVBoxLayout()
-            
-            self.line_edit = QLineEdit()
-            self.line_edit.setEchoMode(QLineEdit.Password)
-            layout.addWidget(self.line_edit)
-            
-            confirm_button = QPushButton("Confirm")
-            confirm_button.setFixedHeight(40)
-            
-            create_password = QPushButton("Create password")
-            create_password.setFixedHeight(20)
-            
-            layout.addWidget(confirm_button)
-            layout.addWidget(create_password)
-            
-            self.line_edit.returnPressed.connect(self.verify_password)
-            confirm_button.clicked.connect(self.verify_password)
-            create_password.clicked.connect(lambda: self.create_password(self.line_edit.text().strip()))
-            self.setLayout(layout)
-
-        def verify_password(self):
-            password = self.line_edit.text().strip()
-            
-            if not password:
-                QMessageBox.warning(self, "Warning", "Please enter a password!")
-                return
-                
-            conn = sqlite3.connect("passwords.db")
-            cur = conn.cursor()
-            cur.execute("SELECT password FROM master_password")
-            row = cur.fetchone()
-            conn.close()
-            
-            if not row:
-                QMessageBox.warning(self, "Warning", "No master password found. Please create one first.")
-                return
-                
-            hashed_password = row[0]
-            if isinstance(hashed_password, str):
-                hashed_password = hashed_password.encode('utf-8')
-
-            if verify_password_hash(password, hashed_password):
-                self.accept()
-            else:
-                QMessageBox.warning(self, "Warning", "Incorrect password!")
-
-            self.get_pass()
-
-        def create_password(self, password: str):
-            if not password:
-                QMessageBox.warning(self, "Warning", "Please enter a password!")
-                return
-                
-            conn = sqlite3.connect("passwords.db")
-            cur = conn.cursor()
-            cur.execute("SELECT password FROM master_password")
-            row = cur.fetchone()
-            
-            if row:
-                QMessageBox.information(self, "PASSWORD ALREADY EXISTS", "A master password already exists.")
-                conn.close()
-                return
-
-            _, hashed_password = generate_password_hash(password)
-            cur.execute("INSERT INTO master_password (password) VALUES (?)", (hashed_password,))
-            conn.commit()
-            conn.close()
-            
-            encrypt_master_key_file(self.line_edit.text().strip())
-
-            QMessageBox.information(self, "Success", "Master password created successfully!")
-            self.line_edit.clear()
-            self.get_pass()
-
-        def get_pass(self):
-            global GLOBAL_PASSWORD
-            GLOBAL_PASSWORD = self.line_edit.text().strip()
-            return GLOBAL_PASSWORD
-
-        def closeEvent(self, event):
-            event.ignore()
-            QMessageBox.information(
-                self, 
-                "Authentication Required", 
-                "You must enter the master password to continue."
-            )
-
-    if GLOBAL_PASSWORD != None:
-        load_master_key_file(GLOBAL_PASSWORD)
-except FileNotFoundError:
-    generate_key()
+        # Input field and label widgets
+        label = QLabel("Enter your master password.")
+        layout.addWidget(label)
+        
+        self.setLayout(layout)
 
 class MainWindow(QMainWindow):
     def __init__(self):
         super().__init__()
 
-        startup = StartupWindow()
-        result = startup.exec_()
-
-        if result != QDialog.Accepted:
-            QApplication.quit()
-            return
-
+        # Window paramaters
         self.setWindowTitle("OwlSafe")
         self.setFixedSize(800, 600)
 
-        main_widget = QWidget()
-        self.setCentralWidget(main_widget)
+        widget = QWidget()
+        self.setCentralWidget(widget)
 
+        # Layout setup
         layout = QVBoxLayout()
         layout.setSpacing(10)
         layout.setContentsMargins(50, 50, 50, 50)
 
         # List widget
-        self.list_widget = QListWidget()
-        for item in self.get_passwords():
-            self.list_widget.addItem(item)
-        layout.addWidget(self.list_widget)
+        list_widget = QListWidget()
+        list_widget.addItem("Test")
+        layout.addWidget(list_widget)
 
-        # Buttons
+        # Button Widgets
         add_button = QPushButton("Add")
         add_button.setFixedHeight(40)
         delete_button = QPushButton("Delete")
         delete_button.setFixedHeight(40)
-
-        test_button = QPushButton("Test")
-        test_button.setFixedHeight(40)
-
         layout.addWidget(add_button)
         layout.addWidget(delete_button)
-        layout.addWidget(test_button)
 
-        main_widget.setLayout(layout)
+        widget.setLayout(layout)
 
-        # Password Line
-        self.line_edit = QLineEdit()
-        layout.addWidget(self.line_edit)
-
-        # Connect buttons
-        add_button.clicked.connect(self.insert_password)
-        delete_button.clicked.connect(lambda: self.remove_password())
-        test_button.clicked.connect(self.print_passwords)
-
-    # Functions
-    def get_passwords(self):
-        key = load_master_key_file(GLOBAL_PASSWORD)
-
-        conn = sqlite3.connect("passwords.db")
-        cur = conn.cursor()
-        cur.execute("SELECT id, password FROM passwords")
-        rows = cur.fetchall()
-        conn.close()
-        
-        items = []
-        for entry_id, encrypted_password in rows:
-            decrypted_password = decrypt_password(encrypted_password, key)
-            item = QListWidgetItem(decrypted_password)
-            item.setData(Qt.UserRole, entry_id)
-            items.append(item)
-            
-        return items
-    
-    # def clean_passwords(self):
-    #     passwords = self.read_file(password_file)
-
-    #     seen = set()
-    #     unique_passwords = []
-        
-    #     for password in passwords:
-    #         password_lower = password.lower()
-    #         if password_lower not in seen:
-    #             seen.add(password_lower)
-    #             unique_passwords.append(password)
-
-    #     return unique_passwords
-    
-    # def remove_duplicate_passwords(self):
-    #     unique_passwords = self.clean_passwords()
-    #     self.write_file(password_file, "\n".join(unique_passwords) + "\n")
-
-    #     self.update_list()
-
-    def update_list(self):
-        self.list_widget.clear()
-        self.list_widget.addItems(self.get_passwords())
-
-    def insert_password(self):
-        password = self.line_edit.text().strip()
-        if not password:
-            return
-
-        key = load_master_key_file(GLOBAL_PASSWORD)
-        encrypted_password = encrypt_password(password, key)
-        
-        conn = sqlite3.connect("passwords.db")
-        cur = conn.cursor()
-        cur.execute("INSERT INTO passwords (password) VALUES (?)", (encrypted_password,))
-        entry_id = cur.lastrowid
-        conn.commit()
-        conn.close()
-
-        item = QListWidgetItem(password)
-        item.setData(Qt.UserRole, entry_id)
-        self.list_widget.addItem(item)
-
-        self.line_edit.clear()
-
-    def remove_password(self):
-        item = self.list_widget.currentItem()
-        if not item:
-            return
-
-        entry_id = item.data(Qt.UserRole)
-
-        # Delete from DB
-        conn = sqlite3.connect("passwords.db")
-        cur = conn.cursor()
-        cur.execute("DELETE FROM passwords WHERE id=?", (entry_id,))
-        conn.commit()
-        conn.close()
-
-        # Remove from list widget
-        self.list_widget.takeItem(self.list_widget.row(item))
-
-app = QApplication([])
-
+app = QApplication(argv)
 window = MainWindow()
 window.show()
 app.exec()
